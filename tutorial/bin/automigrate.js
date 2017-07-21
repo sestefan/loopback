a6c83b94ba30ea9497dd3453c7a9c49d09ebc5ce
Sebastian Estefan <sestefan@binbit.com>
2017-07-21 08:08:55 -0300

    Se borra todo

--- tutorial/bin/automigrate.js
+++
@@ -1,7 +0,0 @@
-var path = require('path');
-
-var app = require(path.resolve(__dirname, '../server/server'));
-var ds = app.datasources.pg;
-ds.autoupdate('evenTypes', function(err) {
-  if (err) throw err;
-});
